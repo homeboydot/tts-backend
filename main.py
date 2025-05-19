d94f368934202ab9efc1eb9701b130933ec9110a
Hakeem Taiwo <70339465+homeboydot@users.noreply.github.com>
2025-05-19 15:48:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -46,5 +46,6 @@ async def health_check():
 if __name__ == "__main__":
     import uvicorn
     import os
+
     port = int(os.environ.get("PORT", 10000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
